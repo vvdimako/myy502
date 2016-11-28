cf4e29d5a9db4f032595df575381a238ccb1c68e
Vassilios V. Dimakopoulos <dimako@cse.uoi.gr>
2016-11-28 10:26:32 +0200

    Added part07 (text files) code.

---
+++ part07-files/p07_03.c
@@ -0,0 +1,16 @@
+/*
+ * part07-files.pdf p.13
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	FILE *fp;
+	
+	fp = fopen("file", "w");
+	fprintf(fp, "%s", "xxx");
+/*	fflush(fp); */
+	while(1)
+		;
+	return 0;
+}
