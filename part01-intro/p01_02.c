90ac2c843ba3e3753eb668c01ec90645fd10bb50
Vassilios V. Dimakopoulos <dimako@cse.uoi.gr>
2017-10-13 17:47:46 +0300

    Added part01 and part02 codes.

---
+++ part01-intro/p01_02.c
@@ -0,0 +1,24 @@
+/*
+ * part01-intro.pdf p.35
+ */
+
+#include <stdio.h>
+
+void main(void) {
+	int x = 95;
+	
+	printf("%d\n", x);    /* 95  */
+	printf("%x\n", x);    /* 5f  */
+	printf("%o\n", x);    /* 137 */
+	printf("%c\n", x);    /* _ */
+	
+	x = 95;
+	x = 0x5F;
+	x = 0137;
+	
+	x = 'd';
+	printf("%d\n", x);    /* 100 */
+	printf("%x\n", x);    /* 64  */
+	printf("%o\n", x);    /* 144 */
+	printf("%c\n", x);    /* d */
+}
