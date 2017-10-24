35388f2532897c341173ba812f12b32c5c2e4881
Vassilios V. Dimakopoulos <dimako@cse.uoi.gr>
2017-10-24 10:44:34 +0300

    Added part03 code.

---
+++ part03-pointers/p03_04.c
@@ -0,0 +1,35 @@
+/*
+ * part03-pointers.pdf p.22
+ */
+
+#include <stdio.h>
+
+/* Function prototypes */
+void init(int *x, int *y);
+void initarr(int x[], int n);
+
+int main(void) {
+	int a, b, array[10], n = 10, i;
+	
+	init(&a, &b);
+	initarr(array, n);
+	
+	printf("a = %d\nb = %d\n", a, b);
+	printf("array = { ");
+	for (i = 0; i < n; i++)
+		printf("%d ", array[i]);
+	printf("}\n");
+	
+	return 0;
+}
+
+void init(int *x, int *y) {
+	*x = 12;
+	*y = 15;
+}
+
+void initarr(int x[], int n) {
+	int i;
+	for (i = 0; i < n; i++)
+		x[i] = i;
+}
