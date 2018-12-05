d6070a25f720fde185bc4fb6a879bf49f5360999
George Z. Zachos <gzachos@cse.uoi.gr>
2018-12-05 02:36:53 +0200

    Added part11-ipc-pipes

---
+++ part11-ipc-pipes/p11_04.c
@@ -0,0 +1,31 @@
+/*
+ * part11-ipc-pipes.pdf p.25
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(void) {
+	int out, bak;
+
+	bak = dup(STDOUT_FILENO);   /* Backup standard output */
+	out = open("file.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IRUSR);
+	if (bak < 0 || out < 0)
+		exit(1);
+
+	printf("Hello world 1\n");
+
+	dup2(out, STDOUT_FILENO);   /* out will be the new standard output */
+	close(out);   /* No longer needed */
+
+	printf("Hello world 2\n");
+
+	dup2(bak, STDOUT_FILENO);   /* Close & restore the original standard output */
+	close(bak);   /* No longer needed */
+
+	printf("Hello world 3\n");
+
+	return 0;
+}
