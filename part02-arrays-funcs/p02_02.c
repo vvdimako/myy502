0a517770419e86824cce2f225b40454357041320
George Z. Zachos <gzachos@cse.uoi.gr>
2019-10-16 10:20:21 +0300

    Added part02-arrays-funcs

---
+++ part02-arrays-funcs/p02_02.c
@@ -0,0 +1,22 @@
+/*
+ * part02-arrays-funcs.pdf p.8
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	char str1[5];
+	char str2[6] = {'b', 'i', 'l', 'l', '\0'};
+	char str3[6] = "maria";
+	char str4[] = "demi";
+	
+	printf("str2 = \"%s\"\n", str2);   /* %s prints a string */
+	printf("str3 = \"%s\"\n", str3);
+	printf("str4 = \"%s\"\n", str4);
+	
+	str2[1] = 'a';    /* ball */
+	
+	printf("str2 = \"%s\"\n", str2);
+	
+	return 0;
+}
---
+++ part02-arrays-funcs/p02_09_c/myscope.h
@@ -0,0 +1,8 @@
+/*
+ * part02-arrays-funcs.pdf p.39
+ */
+
+#define SCALE 2
+
+int f(int param);
+extern int number;
